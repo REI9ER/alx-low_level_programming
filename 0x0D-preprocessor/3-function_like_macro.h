96f9886941c6986c8927778184121a40b2e0ebb3
REI9ER <rei9er@gmail.com>
2023-04-17 14:29:56 -0700

    Function-like macro

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,13 +1,7 @@
-#include <stdio.h>
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
 
-/**
- * main - prints the name of the file
- *
- * Return: Always 0 (Success)
- */
-int ain(void)
-{
-	printf("%s\n", __FILE__);
-	return (0);
-}
+#define ABS(x) ((x) < (0) ? -(x) : (x))
+
+#endif
 
